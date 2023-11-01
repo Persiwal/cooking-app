fec61be1c8384c1ccf30b2c02e09de05e09dc69f
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2023-11-01 11:11:13 +0100

    feat: register api endpoint

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,18 @@
+import prisma from '@/app/_libs/prismadb';
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
+
+const DEFAULT_PASSWORD_SALT = 10;
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    const { name, email, password } = body;
+
+    const hashedPassword = await bcrypt.hash(password, DEFAULT_PASSWORD_SALT)
+
+    const user = await prisma.user.create({
+        data: { name, email, hashedPassword }
+    })
+
+    return NextResponse.json(user)
+}
