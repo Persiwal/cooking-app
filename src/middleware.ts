ea473edf417a4a7f0e255de078f6b2ddb138060d
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2023-11-01 11:05:18 +0100

    feat: integrate next-auth with next-intl in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,46 @@
+import { withAuth } from 'next-auth/middleware';
 import createMiddleware from 'next-intl/middleware';
+import type { NextRequest } from 'next/server';
+import { LOCALES } from './app/_types/locales';
+import { ROUTES } from './app/_types/routes';
 
-export default createMiddleware({
+const locales = [LOCALES.ENGLISH, LOCALES.POLISH]
+const publicPages = [ROUTES.HOME_PAGE, ROUTES.REGISTER_PAGE, ROUTES.LOGIN_PAGE]
+
+const intlMiddleware =  createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'pl'],
-
+  locales: [LOCALES.ENGLISH, LOCALES.POLISH],
+  // localeDetection: false,
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: 'en',
+  defaultLocale: LOCALES.ENGLISH
 });
+
+const authMiddleware = withAuth(
+  function onSuccess(req) {
+    return intlMiddleware(req);
+  },
+  {callbacks: {
+    authorized: ({token}) => token != null
+  },
+  pages: {
+    signIn: ROUTES.LOGIN_PAGE
+  }
+}
+)
+
+export default function middleware (req: NextRequest) {
+   const publicPathnameRegex = RegExp(
+    `^(/(${locales.join('|')}))?(${publicPages.join('|')})?/?$`,
+    'i'
+  );
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+ 
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return (authMiddleware as any)(req);
+  }
+}
 
 export const config = {
   // Skip all paths that should not be internationalized. This example skips
