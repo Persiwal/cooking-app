64549e9e69950cea117aeae53d33976ce69e9dd3
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2023-11-10 12:21:06 +0100

    feat: loadingSpinner component

---
+++ src/app/_components/ui/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,13 @@
+import styles from './LoadingSpinner.module.scss';
+
+const LoadingSpinner = () => {
+  return (
+    <div className={styles.spinnerContainer}>
+      <div className={styles.spinner}></div>
+    </div>
+  )
+
+};
+
+export default LoadingSpinner;
+
