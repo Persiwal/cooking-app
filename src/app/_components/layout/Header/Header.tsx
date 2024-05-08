0643049517fad164fcdedb65863fdee4073d92b7
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2024-05-08 16:03:11 +0200

    feat: mobile menu

--- src/app/_components/layout/Header/Header.tsx
+++ src/app/_components/layout/Header/Header.tsx
@@ -4,22 +4,32 @@ import { Container, Flex } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react';
 import Logo from '../../ui/Logo/Logo';
 import styles from './Header.module.scss';
-import HeaderActions from './HeaderActions/HeaderActions';
-import MainNav from './MainNav/MainNav';
+import MainNav from './desktop-nav/DesktopNav';
+import HeaderActions from './header-actions/HeaderActions';
+import MobileMenu from './mobile-menu/MobileMenu';
 
 const Header = () => {
   const session = useSession();
-  const authState =
-    session && session.data ? 'authenticated' : 'unauthenticated';
 
   return (
-    <header className={styles.container}>
-      <Container size="4">
-        <Flex align="center" justify="between">
+    <header className={styles.header}>
+      <Container size="4" px="4" py="2">
+
+        {/* mobile */}
+        <Flex align="center" justify="between" className={styles.mobile}>
           <Logo />
-          <MainNav type={authState} />
-          <HeaderActions type={authState} session={session.data} />
+          <MobileMenu session={session.data} />
         </Flex>
+        {/* mobile */}
+
+        {/* desktop */}
+        <Flex align="center" justify="between" className={styles.desktop}>
+          <Logo />
+          <MainNav />
+          <HeaderActions session={session.data} />
+        </Flex>
+        {/* desktop */}
+
       </Container>
     </header>
   );
---
+++ src/app/_components/layout/Header/mobile-menu/MobileMenu.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import useRoutes from '@/app/_hooks/useRoutes';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import {
+    HamburgerMenuIcon
+} from '@radix-ui/react-icons';
+import { IconButton } from '@radix-ui/themes';
+import { Session } from 'next-auth';
+import Link from "next/link";
+import HeaderActions from '../header-actions/HeaderActions';
+import styles from "./MobileMenu.module.scss";
+
+
+type Props = {
+    session: Session
+};
+
+const MobileMenu: React.FC<Props> = ({ session }) => {
+    const routes = useRoutes();
+
+    return (<DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+            <IconButton radius='full' className={styles.hamburgerButton}>
+                <HamburgerMenuIcon />
+            </IconButton >
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Portal>
+            <DropdownMenu.Content
+                className={styles.dropdownMenuContent}
+            >
+                {routes.map(route => {
+                    return (
+                        <DropdownMenu.Item key={route.path} className={styles.dropdownMenuItem}>
+                            <Link
+                                href={route.path}
+                                className={`${styles.navLink} ${route.active && styles.active}`}
+                            >
+                                {route.label}
+                            </Link>
+                        </DropdownMenu.Item>
+                    )
+                })}
+
+                <DropdownMenu.Separator className={styles.dropdownMenuSeparator} />
+
+                <HeaderActions session={session} isMobile={true} />
+
+                <DropdownMenu.Arrow className={styles.dropdownMenuArrow} />
+            </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+    );
+}
+
+export default MobileMenu;
